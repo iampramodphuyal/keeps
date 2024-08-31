622e9a6be5e25ab4e9161b6a422532eeba2d1ff8
Pramod <pramod.phuyal@grepsr.com>
2024-09-01 01:53:22 +0545

    init- update

--- commands/add.py
+++ commands/add.py
@@ -1,25 +1,25 @@
 import typer
-# from database.database import insert
+from database.database import insert
 
 addApp = typer.Typer()
 
 @addApp.command()
 def config():
-    configName = typer.prompt("Assign a Name for this credential: ")
+    configName = typer.prompt("Assign a Name for this credential")
     if not configName.strip():
         typer.secho("Error: A name is must", fg=typer.colors.RED)
         raise typer.Exit(code=1)
-    accessKey = typer.prompt("Enter AccessKey: ")
+    accessKey = typer.prompt("Enter AccessKey")
     if not accessKey.strip():
         typer.secho("Error: Accesskey is empty", fg=typer.colors.RED)
         raise typer.Exit(code=1)
-    secretKey = typer.prompt("Enter SecretKey: ")
+    secretKey = typer.prompt("Enter SecretKey")
     if not secretKey.strip():
         typer.secho("Error: SecretKey is empty", fg=typer.colors.RED)
         raise typer.Exit(code=1)
-    region = typer.prompt("Enter Region(eu-central-1): ", default="eu-central-1")
-    bucketName = typer.prompt("Enter Bucket Name: ")
-    # insert(configName, secretKey, accessKey, region, bucketName)
+    region = typer.prompt("Enter Region(eu-central-1)", default="eu-central-1")
+    bucketName = typer.prompt("Enter Bucket Name", default=None)
+    insert(configName, secretKey, accessKey, region, bucketName)
 
 if __name__ == "__main__":
     addApp()
--- database/database.py
+++ database/database.py
@@ -1,11 +1,15 @@
 import sqlite3
 import typer
 
+DATABASE_NAME = 'creds.db'
+DATABASE_TABLE = 'creds'
+
+
 def init_db():
-    conn = sqlite3.connect("users.db")
+    conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
-    query = '''CREATE TABLE IF NOT EXISTS creds(
-    id integer PRIMARY KEY,
+    query = f'''CREATE TABLE IF NOT EXISTS {DATABASE_TABLE}(
+    id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT,
     accesskey TEXT,
     secretkey TEXT,
@@ -16,19 +20,31 @@ def init_db():
     conn.commit()
     return conn, cursor
 
+def drop_db():
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+    query = f'''DROP TABLE {DATABASE_TABLE}'''
+    try:
+        cursor.execute(query)
+        conn.commit()
+        conn.close()
+        typer.secho(f"Table : {DATABASE_TABLE}, Deleted Successfully", fg=typer.colors.GREEN)
+    except:
+        typer.secho(f"Failed to drop table: {DATABASE_TABLE}", fg=typer.colors.RED)
+
 def insert(configName: str, accesskey: str, secretkey: str, region: str, bucket_name: str | None):
-    query = f'''
-    INSERT INTO users (name,accesskey,secretkey, region,bucket_name) VALUES ({configName}, {accesskey}, {secretkey}, {region}, {bucket_name})
-    '''
-    conn, cursor = init_db()
+    query = f"""
+    INSERT INTO {DATABASE_TABLE} (name,accesskey,secretkey, region,bucket_name) VALUES ('{configName}', '{accesskey}', '{secretkey}', '{region}', '{bucket_name}');
+    """
+    print(f"query :: {query}")
     try:
         conn, cursor = init_db()
         cursor.execute(query)
         conn.commit()
+        conn.close()
         typer.secho("Credential Saved Successfully!", fg=typer.colors.GREEN)
     except:
         typer.secho("Failed to Save credentials!", fg=typer.colors.RED)
-    conn.close()
 
 def get_all_configs():
     conn, cursor = init_db()
@@ -39,3 +55,4 @@ def get_all_configs():
     rows = cursor.fetchall()
     conn.close()
     return rows
+
